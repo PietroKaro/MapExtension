30f2889e2ebc0b9f1691a505eff7bb1b48a3320c
Pietro Carozzi <pietro.karo@gmail.com>
2021-05-22 11:47:42 +0200

    Added loader class

---
+++ VanillaExternalLocations/ConfigLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanillaExternalLocations
+{
+    class ConfigLoader
+    {
+        static ConfigLoader instance;
+
+        private ConfigLoader() {}
+
+        public ConfigLoader GetLoader()
+        {
+            if (instance == null)
+            {
+                instance = new ConfigLoader();
+                return instance;
+            }
+            return null;
+        }
+    }
+}
