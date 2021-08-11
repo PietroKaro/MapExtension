48a57591c232b97f09bb87f4cccc9d881afbd2df
Pietro Carozzi <pietro.karo@gmail.com>
2021-08-12 00:46:39 +0200

    Added funcs to modify water

--- VanillaExternalLocations/MainScript.cs
+++ VanillaExternalLocations/MainScript.cs
@@ -29,7 +29,7 @@ namespace VanillaExternalLocations
         readonly string[] _YachtVespucciIPL = { "hei_yacht_heist", "hei_yacht_heist_enginrm", "hei_yacht_heist_Lounge", "hei_yacht_heist_Bridge", "hei_yacht_heist_Bar", "hei_yacht_heist_Bedrm", "hei_yacht_heist_DistantLights", "hei_yacht_heist_LODLights" };
         readonly string[] _YachtPaletoIPL = { "gr_heist_yacht2", "gr_heist_yacht2_bar", "gr_heist_yacht2_bedrm", "gr_heist_yacht2_bridge", "gr_heist_yacht2_enginrm", "gr_heist_yacht2_lounge" };
 
-        byte _GlobalWaterType // Get/Set if the ocean has to be modified for Cayo Perico island (to avoid high waves).
+        private byte GlobalWaterType // Get/Set if the ocean has to be modified for Cayo Perico island (to avoid high waves).
         {
             get
             {
@@ -42,7 +42,7 @@ namespace VanillaExternalLocations
         }
 
         Blip _BCayoPerico, _BAircraftCarrier, _BVespucciY, _BPaleyoY;
-        bool _AreLocationsLoaded, _CayoPericoCheck, _YVespucciCheck, _YPaletoCheck, CarrierCheck;
+        bool _AreLocationsLoaded, _CayoPericoCheck, _YVespucciCheck, _YPaletoCheck, _CarrierCheck, ShipOcean;
         
         public MainScript()
         {
@@ -50,6 +50,8 @@ namespace VanillaExternalLocations
             KeyUp += OnKeyUp;
             KeyDown += OnKeyDown;
             Aborted += OnAborted;
+            ConfigLoader loader = ConfigLoader.GetLoader();
+            (_CayoPericoCheck, _CarrierCheck, _YVespucciCheck, _YPaletoCheck) = loader.GetActiveLocations(BaseDirectory + @"\VELConfig.xml");
         }
 
         void OnTick(object sender, EventArgs args)
@@ -59,6 +61,7 @@ namespace VanillaExternalLocations
                 InitializeLocations();
                 _AreLocationsLoaded = true;
             }
+            //OceanControl();
         }
 
         void OnKeyUp(object sender, KeyEventArgs args)
@@ -71,30 +74,41 @@ namespace VanillaExternalLocations
             // debug
             switch (args.KeyCode)
             {
-                case Keys.NumPad1:
-                    _GlobalWaterType = 1;
+                case Keys.F6:
+                    GlobalWaterType = 1;
                     break;
-                case Keys.NumPad3:
-                    _GlobalWaterType = 0;
+                case Keys.F7:
+                    GlobalWaterType = 0;
                     break;
-                case Keys.NumPad4:
+                case Keys.F8:
                     World.Weather = Weather.ThunderStorm;
                     break;
-                case Keys.NumPad5:
+                case Keys.F9:
                     ToggleShipWater(true);
+                    GTA.UI.Screen.ShowSubtitle("ship water true");
                     break;
-                case Keys.NumPad6:
+                case Keys.F10:
                     ToggleShipWater(false);
+                    GTA.UI.Screen.ShowSubtitle("ship water false");
                     break;
-                case Keys.NumPad2:
+                case Keys.F11:
                     Notification.Show(Game.Player.Character.Position.ToString());
+                    Notification.Show(Vector3.Distance2D(Game.Player.Character.Position, AIRCRAFT_CARRIER_POS).ToString());
+                    Notification.Show(_CayoPericoCheck.ToString() + " " + _CarrierCheck.ToString() + " " + _YVespucciCheck.ToString() + " " + _YPaletoCheck.ToString());
                     break;
-                case Keys.NumPad7:
-                    
-                    break;
-                case Keys.NumPad8:
+                case Keys.F12:
                     World.CurrentTimeOfDay = new TimeSpan(12, 00, 00);
                     World.Weather = Weather.ExtraSunny;
+                    break;
+                case Keys.D0:
+                    foreach(Vehicle v in World.GetAllVehicles())
+                    {
+                        v.Delete();
+                    }
+                    foreach (Ped p in World.GetAllPeds())
+                    {
+                        p.Delete();
+                    }
                     break;
             }
         }
@@ -105,7 +119,7 @@ namespace VanillaExternalLocations
             _BAircraftCarrier.Delete();
             _BVespucciY.Delete();
             _BPaleyoY.Delete();
-            _GlobalWaterType = 0;
+            GlobalWaterType = 0;
             ToggleShipWater(false);
         }
         
@@ -114,30 +128,42 @@ namespace VanillaExternalLocations
             Function.Call(Hash._LOAD_MP_DLC_MAPS);
             Yield();
             Notification.Show("Mappa online caricata"); // debug
-            while (!IsLocationPresent(_CayoPericoIPL))
+            if (_CayoPericoCheck)
             {
-                ToggleLocation(_CayoPericoIPL, true);
-                Yield();
+                while (!IsLocationPresent(_CayoPericoIPL))
+                {
+                    ToggleLocation(_CayoPericoIPL, true);
+                    Yield();
+                }
+                _BCayoPerico = CreateIPLBlip(CAYO_PERICO_POS, BlipSprite.PointOfInterest, 1.0f, "Cayo Perico");
             }
-            _BCayoPerico = CreateIPLBlip(CAYO_PERICO_POS, BlipSprite.PointOfInterest, 1.0f, "Cayo Perico");
-            while (!IsLocationPresent(_AircraftCarrierIPL))
+            if (_CarrierCheck)
             {
-                ToggleLocation(_AircraftCarrierIPL, true);
-                Yield();
+                while (!IsLocationPresent(_AircraftCarrierIPL))
+                {
+                    ToggleLocation(_AircraftCarrierIPL, true);
+                    Yield();
+                }
+                _BAircraftCarrier = CreateIPLBlip(AIRCRAFT_CARRIER_POS, BlipSprite.Airport, 1.0f, "Aircraft Carrier");
             }
-            _BAircraftCarrier = CreateIPLBlip(AIRCRAFT_CARRIER_POS, BlipSprite.Airport, 1.0f, "Aircraft Carrier");
-            while (!IsLocationPresent(_YachtPaletoIPL))
+            if (_YVespucciCheck)
             {
-                ToggleLocation(_YachtPaletoIPL, true);
-                Yield();
+                while (!IsLocationPresent(_YachtVespucciIPL))
+                {
+                    ToggleLocation(_YachtVespucciIPL, true);
+                    Yield();
+                }
+                _BVespucciY = CreateIPLBlip(YACHT_VESPUCCI_POS, BlipSprite.Yacht, 1.0f, "Vespucci Yacht");
             }
-            _BVespucciY = CreateIPLBlip(YACHT_VESPUCCI_POS, BlipSprite.Yacht, 1.0f, "Vespucci Yacht");
-            while (!IsLocationPresent(_YachtVespucciIPL))
+            if (_YPaletoCheck)
             {
-                ToggleLocation(_YachtVespucciIPL, true);
-                Yield();
+                while (!IsLocationPresent(_YachtPaletoIPL))
+                {
+                    ToggleLocation(_YachtPaletoIPL, true);
+                    Yield();
+                }
+                _BPaleyoY = CreateIPLBlip(YACHT_PALETO_POS, BlipSprite.Yacht, 1.0f, "Paleto Yacht");
             }
-            _BPaleyoY = CreateIPLBlip(YACHT_PALETO_POS, BlipSprite.Yacht, 1.0f, "Paleto Yacht");
             Notification.Show("mappe caricate"); // debug
         }
 
@@ -186,6 +212,52 @@ namespace VanillaExternalLocations
             else
             {
                 Function.Call(Hash.RESET_DEEP_OCEAN_SCALER);
+            }
+        }
+
+        void OceanControl()
+        {
+            if (_CayoPericoCheck)
+            {
+                float distance = Vector3.Distance2D(Game.Player.Character.Position, CAYO_PERICO_POS);
+                if (GlobalWaterType != 1 && distance < 2500.0f)
+                {
+                    GlobalWaterType = 1;
+                    GTA.UI.Screen.ShowSubtitle("Oceano CP");
+                }
+                else if (distance > 2500.0f && GlobalWaterType == 1)
+                {
+                    GlobalWaterType = 0;
+                    GTA.UI.Screen.ShowSubtitle("Oceano LS");
+                }
+            }
+            bool nearCarrier = IsPlayerNearShip(AIRCRAFT_CARRIER_POS);
+            bool nearVY = IsPlayerNearShip(YACHT_VESPUCCI_POS);
+            bool nearPY = IsPlayerNearShip(YACHT_PALETO_POS);
+            if (!ShipOcean && ((_CarrierCheck && nearCarrier) || (_YVespucciCheck && nearVY) || (_YPaletoCheck && nearPY)))
+            {
+                ToggleShipWater(true);
+                ShipOcean = true;
+                GTA.UI.Screen.ShowSubtitle("Oceano ship");
+            }
+            else if (ShipOcean && !nearCarrier && !nearVY && !nearPY)
+            {
+                ToggleShipWater(false);
+                ShipOcean = false;
+                GTA.UI.Screen.ShowSubtitle("Oceano normale");
+            }
+        }
+
+        bool IsPlayerNearShip(Vector3 shipPos)
+        {
+            float distance = Vector3.Distance2D(Game.Player.Character.Position, shipPos);
+            if (distance < 373.0f)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
